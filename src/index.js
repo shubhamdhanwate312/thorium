83b350f79479fe84b2f4375ef936166f1e0716fa
Shubham <shubhamdhanwate312@gmail.com>
2022-03-06 14:12:20 +0530

    middleware

--- src/index.js
+++ src/index.js
@@ -6,9 +6,20 @@ const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(function (req,res,next){
+let currentDate=new Date()
+let dayMonth=currentDate.getDate()
+let monthB=currentDate.getMonth()
+let yearC=currentDate.getFullYear()
+let time=currentDate.toUTCString().substring(17,25)
+const datestring=yearC+"-"+(monthB+1)+"-"+dayMonth+" "+time
+let ipADD=req.socket.remoteAddress //connection object also use but this object can be Deprecated
+console.log(datestring,ipADD,req.path)
+next();
 
+})
 
-mongoose.connect("mongodb+srv://functionup-cohort:G0Loxqc9wFEGyEeJ@cluster0.rzotr.mongodb.net/Pritesh8769811-DB?retryWrites=true&w=majority", {
+mongoose.connect("mongodb+srv://functionup-cohort:G0Loxqc9wFEGyEeJ@cluster0.rzotr.mongodb.net/Shubham-DB?retryWrites=true&w=majority", {
     useNewUrlParser: true
 })
 .then( () => console.log("MongoDb is connected"))
