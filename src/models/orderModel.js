ce96123a90bffd7de05353637f1025479aa08f6a
shubhamdhanwate312 <97374437+shubhamdhanwate312@users.noreply.github.com>
2022-03-07 23:51:02 +0530

    Update orderModel.js

--- src/models/orderModel.js
+++ src/models/orderModel.js
@@ -3,13 +3,13 @@ const ObjectId = mongoose.Schema.Types.ObjectId
 const orderSchema = new mongoose.Schema( {
 
  
-        "userId":{type: ObjectId, ref:"User"},
-        "productId":{type: ObjectId, ref: "Product"},
-       "amount":Number,
-      "isFreeAppUser": {
+        userId:{type: ObjectId, ref:"User"},
+        productId:{type: ObjectId, ref: "Product"},
+       amount:Number,
+      isFreeAppUser: {
           type:Boolean 
         }, 
-        "date":String
+        date:String
         }, { timestamps: true });
 
 
