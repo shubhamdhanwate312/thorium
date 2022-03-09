93db5f4f5f8ee006d0da3ebc9caa67b5171e7cec
Shubham <shubhamdhanwate312@gmail.com>
2022-03-09 22:45:41 +0530

    dsafgf

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -1,29 +1,39 @@
-let authenticate=function(req,res,next){
-      //check the token in request header
+let jwt=require('jsonwebtoken')
+let authenticate = function (req, res, next) {
+    //check the token in request header
     //validate this token
 
     let xAuthToken = req.headers["x-auth-token"]
-    if( xAuthToken != undefined){
+    if (xAuthToken != undefined) {
+
         console.log("done")
         next()
     }
-    else{
+    else {
         res.send("request is missing a mandatory header")
     }
 }
 
 
-let authorise=function(req,res,next){
-     // comapre the logged in user's id and the id in request
-     let userToBeModified = req.params.userId
-      if(userToBeModified != userLoggedIn) {
+let authorise = function (req, res, next) {
+    // comapre the logged in user's id and the id in request
+    let token = req.headers["x-auth-token"]
+    if (!token) return res.send({ status: false, msg: "token must be present in the request header" })
+    let decodedToken = jwt.verify(token, 'shubham-thorium')
+
+    if (!decodedToken) return res.send({ status: false, msg: "token is not valid" })
+    let userToBeModified = req.params.userId
+    let userLoggedIn = decodedToken.userId
+    if (userToBeModified != userLoggedIn) { 
+         res.send("User logged is not allowed to modify the requested users data")
+    }
         console.log("done")
         next()
-    }
-    else{
-        res.send("User logged is not allowed to modify the requested users data")
-    }
 }
 
+      
+    
+
+
 module.exports.authenticate = authenticate;
-module.exports.authorise= authorise;
+module.exports.authorise = authorise;
--- src/routes/route.js
+++ src/routes/route.js
@@ -12,12 +12,12 @@ router.post("/users", userController.createUser)
 router.post("/login", userController.loginUser)
 
 //The userId is sent by front end
-router.get("/users/:userId",middleware.authenticate,userController.getUserData)
+router.get("/users/:userId",middleware.authenticate,middleware.authorise,userController.getUserData)
 
-router.put("/users/:userId",middleware.authenticate, userController.updateUser)
+router.put("/users/:userId",middleware.authenticate,middleware.authorise, userController.updateUser)
 
-router.delete("/users/:userId",middleware.authenticate, userController.deleteUserData)
+router.delete("/users/:userId",middleware.authenticate, middleware.authorise,userController.deleteUserData)
 
-router.post("/users/:userId/posts",middleware.authenticate, userController.postMessage)
+router.post("/users/:userId/posts",middleware.authenticate,middleware.authorise, userController.postMessage)
 
 module.exports = router;
