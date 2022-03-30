961140ebbda81762c2b3b87c2ef29384da85a84c
nasirhussain <nasirhussain7878@gmail.com>
2022-03-31 02:06:20 +0530

    project3

--- src/controllers/reviewController.js
+++ src/controllers/reviewController.js
@@ -1,8 +1,182 @@
-const { type } = require('express/lib/response')
 const reviewModel = require('../models/reviewModel')
+const booksModel = require('../models/bookModel')
+const ObjectId = require("mongoose").Types.ObjectId
 
-const isValid = function(value){
-    if( typeof value== 'undefined' || typeof value == 'null') return false
-    if( typeof value== 'string' && value.trim().length == 0) return false
+
+//✅validation........................................................
+
+const isValidRequestBody = function (requestBody) {
+    return Object.entries((requestBody).length > 0)
+}
+const isValid = function (value) {
+    if (typeof value == undefined || value == null) return false
+    if (typeof value === 'string' && value.trim().length === 0) return false
     return true
 }
+
+
+//✅Creat Review.......................................................
+const creatReview = async function (req, res) {
+    try {
+        const data = req.body
+        const bookId = req.params.bookId
+        if (!ObjectId(bookId)) return res.status(400).send({ status: false, message: "please provide valid id" })
+
+        if (!isValidRequestBody) {
+            return res.status(400).send({ status: false, message: "please provide review data" })
+        }
+        const { review, rating, reviewedAt, reviewedBy } = data
+        if (!isValid(rating)) return res.status(400).send({ status: false, message: "please provide review rating" })
+
+
+        if (!isValid(review)) return res.status(400).send({ status: false, message: "please provide review " })
+
+        if (!isValid(reviewedBy)) return res.status(400).send({ status: false, message: "please provide reviewrs name" })
+
+        if (!isValid(bookId)) return res.status(400).send({ status: false, message: "please provide book id" })
+        //book does not exit
+        //validating rating
+
+        if (rating < 1) return res.status(400).send({ status: false, message: "rating must be greater than 1" })
+        if (rating > 5) return res.status(400).send({ status: false, message: "rating must be less than 5" })
+
+        //validating reviewdAt
+        data.reviewedAt = Date.now();
+
+        const reviewDetails = await reviewModel.create(data)
+
+        const countReview = await reviewModel.findByIdAndUpdate({ _id: bookId, isDeleted: false }, { $inc: { reviews: 1 } })
+        const details = {
+            _id: reviewDetails._id,
+            bookId: reviewDetails.bookId,
+            reviewedBy: reviewDetails.reviewedBy,
+            reviewedAt: reviewDetails.reviewedAt,
+            rating: reviewDetails.rating,
+            review: reviewDetails.review
+
+        }
+
+        return res.status(201).send({ status: true, message: "success", data: details })
+
+    }
+    catch (err) {
+        console.log(err)
+        return res.status(500).send({ status: false, message: "error" })
+    }
+}
+
+
+
+
+
+//✅Update by Review ID........................................................................
+
+const updatebyReviewId = async function (req, res) {
+
+    try {
+        const bookId = req.params.bookId
+        const reviewId = req.params.reviewId
+        const Updation = req.body
+
+        if (!(/^[0-9a-fA-F]{24}$/.test(bookId))) {
+            return res.status(400).send({ status: false, message: 'please provide valid bookId' })
+        }
+
+        if (!(/^[0-9a-fA-F]{24}$/.test(reviewId))) {
+            return res.status(400).send({ status: false, message: 'please provide valid reviewId' })
+        }
+        const book = await booksModel.findOne({_id:bookId,isDeleted:false, deletedAt:null})
+        const reviews = await reviewModel.findOne({ _id: reviewId, isDeleted: false })
+        if(!book){
+            return res.status(404).send("no book found")
+        }
+
+        if (!reviews) {
+            return res.status(404).send({ status: false, message: "No data found" })
+        }
+
+        if (!isValidRequestBody(Updation)) {
+            return res.status(400).send({ status: false, message: 'please provide data for updation' })
+        }
+
+        const { review, rating, reviewedBy } = Updation
+
+        if (!isValid(review)) {
+            return res.status(400).send({ status: false, message: 'please provide review' })
+
+        }
+
+        if (!isValid(rating)) {
+            return res.status(400).send({ status: false, message: 'please provide rating' })
+        }
+
+        if (!isValid(reviewedBy)) {
+            return res.status(400).send({ status: false, message: 'please provide reviewersname' })
+
+        }
+        if (!isValid(bookId)) return res.status(400).send({ status: false, message: "please provide book id" })
+
+        if (!isValid(reviewId)) return res.status(400).send({ status: false, message: "please provide review id" })
+        
+        const updateData = { review, rating, reviewedBy }
+
+        const updated = await reviewModel.findOneAndUpdate({ _id: reviewId }, { ...updateData }, { new: true })
+
+        return res.status(200).send({ status: true, message: "review updated successfully", data: updated })
+    }
+    catch (err) {
+        console.log(err)
+        res.status(500).send({ status: false, msg: err.message })
+    }
+};
+
+
+
+//✅Deleted by review Id............................................................................
+
+let deleteByReviewId = async function (req, res) {
+
+    try {
+
+        const bookId = req.params.bookId
+        const Reviews = req.params.reviewId
+        if (!bookId) {
+            return res.status(400).send({ status: false, message: "bookId is required in path params" })
+        }
+
+        if (!isValid(bookId)) {
+            return res.status(400).send({ status: false, message: `enter a valid bookId` })
+        }
+        if (!Reviews) {
+            return res.status(400).send({ status: false, message: "reviewId is required in path params" })
+        }
+
+        if (!isValid(Reviews)) {
+            return res.status(400).send({ status: false, message: `enter a valid reviewId` })
+        }
+
+        const book = await booksModel.findOne({ _id: bookId, isDeleted: false, deletedAt: null })
+
+        const review = await reviewModel.findOne({ _id: Reviews, isDeleted: false, deletedAt: null })
+
+        if (!book) {
+            return res.status(404).send({ status: false, message: "no book found by this ID" })
+        }
+        if (!review) {
+            return res.status(404).send({ status: false, message: "no book found by this ID" })
+        }
+
+        const deletereview = await reviewModel.findByIdAndUpdate(review,{ $set: { isDeleted: true, deletedAt: Date.now() } }, { new: true })
+
+        res.status(200).send({ status: true, message: "review deleted successfully" })
+
+
+    } catch (err) {
+        res.status(500).send({ error: err.message })
+    }
+}
+
+
+module.exports.creatReview = creatReview
+module.exports.updatebyReviewId = updatebyReviewId
+module.exports.deleteByReviewId = deleteByReviewId
--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,20 +1,20 @@
 const userModel = require("../models/userModel")
-const jwt= require("jsonwebtoken")
+const jwt = require("jsonwebtoken")
 
 
-const isValid = function(value) {
-    if(typeof value === 'undefined' || value === null) return false
-    if(typeof value === 'string' && value.trim().length === 0) return false
-    if(typeof value === 'number' && value.toString().trim().length === 0) return false
+const isValid = function (value) {
+    if (typeof value === 'undefined' || value === null) return false
+    if (typeof value === 'string' && value.trim().length === 0) return false
+    if (typeof value === 'number' && value.toString().trim().length === 0) return false
     return true;
 }
 
-const isValidTitle = function(title) {
+const isValidTitle = function (title) {
     return ['Mr', 'Mrs', 'Miss'].indexOf(title) !== -1
 }
 
 
-const isValidRequestBody = function(requestBody) {
+const isValidRequestBody = function (requestBody) {
     return Object.keys(requestBody).length > 0
 }
 
@@ -23,101 +23,133 @@ const isValidRequestBody = function(requestBody) {
 const createUser = async function (req, res) {
     try {
         const userBody = req.body;
-        if(!isValidRequestBody(userBody)) {
-            return res.status(400).send({status: false, message: 'Please provide user details'})
+        if (!isValidRequestBody(userBody)) {
+            return res.status(400).send({ status: false, message: 'Please provide user details' })
         }
 
-        const {title, name, phone, email, password, address} = userBody;
-        
-        if(!isValid(title)) {
-            return res.status(400).send({status: false, message: 'Title is required'})
-        }
-        
-        if(!isValidTitle(title)) {
-            return res.status(400).send({status: false, message: 'Title is not Valid'})
+        const { title, name, phone, email, password, address } = userBody;
+
+        if (!isValid(title)) {
+            return res.status(400).send({ status: false, message: 'Title is required' })
         }
 
-
-        if(!isValid(name)) {
-            return res.status(400).send({status: false, message: 'Name is required'})
+        if (!isValidTitle(title)) {
+            return res.status(400).send({ status: false, message: 'Title is not Valid' })
         }
 
-        if(!isValid(phone)) {
-            return res.status(400).send({status: false, message: 'Phone number is required'})
+        if (!isValid(name)) {
+            return res.status(400).send({ status: false, message: 'Name is required' })
         }
-        
+
+        if (!isValid(phone)) {
+            return res.status(400).send({ status: false, message: 'Phone number is required' })
+        }
+
         if (!(/^([+]\d{2})?\d{10}$/.test(userBody.phone))) {
             return res.status(400).send({ status: false, msg: 'please provide a valid moblie Number' })
         }
 
-        if(!isValid(email)) {
-            return res.status(400).send({status: false, message: 'Email is required'})
+        if (!isValid(email)) {
+            return res.status(400).send({ status: false, message: 'Email is required' })
         }
-        
+
         if (!(/^\w+([\.-]?\w+)@\w+([\. -]?\w+)(\.\w{2,3})+$/.test(userBody.email))) {
             return res.status(400).send({ status: false, msg: "Please provide a valid email" })
         }
 
-        if(!isValid(password)) {
-            return res.status(400).send({status: false, message: 'Password is required'})
+        if (!isValid(password)) {
+            return res.status(400).send({ status: false, message: 'Password is required' })
         }
         // minumum 8 max 15 
         if (!(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[#$@!%&*?])[A-Za-z\d#$@!%&*?]{8,15}$/.test(userBody.password))) {
 
             return res.status(400).send({ status: false, message: ' Min 1 uppercase letter,Min 1 lowercase letter,Min 1 special character,Min 1 number,Min 8 characters,Max 15 characters' })
-      
-          }
-      
-        
-        const duplicatePhone = await userModel.findOne({phone});
-        if(duplicatePhone) {
-            return res.status(400).send({status: false, message: `${phone} phone number is already registered`})
-        }
-        
-        const duplicateEmail = await userModel.findOne({email});
 
-        if(duplicateEmail) {
-            return res.status(400).send({status: false, message: `${email} email address is already registered`})
         }
 
-        const userData = {title, name, phone, email, password, address}
+
+        const duplicatePhone = await userModel.findOne({ phone });
+        if (duplicatePhone) {
+            return res.status(400).send({ status: false, message: `${phone} phone number is already registered` })
+        }
+
+        const duplicateEmail = await userModel.findOne({ email });
+
+        if (duplicateEmail) {
+            return res.status(400).send({ status: false, message: `${email} email address is already registered` })
+        }
+
+        const userData = { title, name, phone, email, password, address }
         const createdUser = await userModel.create(userData);
 
-        return res.status(201).send({status: true, message: 'User successfully created', data: createdUser});
-    
+        return res.status(201).send({ status: true, message: 'User successfully created', data: createdUser });
+
     } catch (error) {
-        return res.status(500).send({status: false, message: error.message});
+        return res.status(500).send({ status: false, message: error.message });
     }
 }
 
 
 //✅ LOGIN USER
+
 const loginUser = async function (req, res) {
+
     try {
-        let userName = req.body.email
-        let password = req.body.password
-        if (!(userName && password)) {
-            res.status(400).send({ msg: "BAD REQUEST" })
-        } else {
-            let User = await userModel.findOne({ email: userName, password: password })
-            if (!User)
-                return res.status(403).send({ msg: "Cannot login as userName and password not matched" });
-            let iat= Math.floor(Date.now() / 1000)
-            let token = jwt.sign({ userId: User._id,exp: iat + (60)}, "Nasir-hussain")
-            res.setHeader("x-auth-token", token);
-            res.status(201).send({ msg: "your login is successfull", data: token })
+  
+      let body = req.body
+  
+      if (Object.keys(body) != 0) {
+        let userName = req.body.email;
+        let passwords = req.body.password;
+        if (!(userName || passwords)) {
+          return res.status(400).send({ status: false, message: "user does not exit" })
         }
+  
+        if (!(/^\w+([\.-]?\w+)@\w+([\. -]?\w+)(\.\w{2,3})+$/.test(userName))) { return res.status(400).send({ status: false, message: "Please provide a valid email" }) }
+  
+        if (!(/^(?=.[a-z])(?=.[A-Z])(?=.\d)(?=.[#$@!%&?])[A-Za-z\d#$@!%&?]{8,15}$/.test(passwords))) {
+  
+          return res.status(400).send({ status: false, message: 'Please provide a valid password' })
+  
+        }
+  
+  
+        let user = await userModel.findOne({ email: userName, password: passwords });
+  
+        if (!user) {
+  
+          return res.status(400).send({
+            status: false,
+            ERROR: "username or the password is not corerct",
+          });
+        }
+  
+        let token = jwt.sign(
+          {
+            userId: user._id,
+            iat: Math.floor(Date.now() / 1000),
+            exp: Math.floor(Date.now() / 1000) + 60 * 60
+  
+          }, "TmySecretK#key...$$@@"
+  
+        );
+        console.log(token);
+        res.status(200).setHeader("x-api-key", token);
+        return res.status(201).send({ status: "LoggedIn", message: 'Success', TOKEN: token });
+      }
+  
+      else { return res.status(400).send({ ERROR: "Bad Request" }) }
+  
     }
-    catch (error) {
-        console.log("This is the error:", error.message)
-        res.status(500).send({ msg: "server error", err: error })
+    catch (err) {
+  
+      return res.status(500).send({ ERROR: err.message })
     }
-
-}
-
+  
+  };
 
 module.exports.loginUser = loginUser
-module.exports.createUser= createUser;
+module.exports.createUser = createUser;
 
 
 
